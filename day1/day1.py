82202a666f25ce19d670f3d521d87038fe2540c9
Anders Åstrandd <anders@teknodromen.se>
2022-01-02 16:48:30 +0100

    Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
-from ..utils import io
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from utils import io
 
 
-def part1(filename):
-    lines = io.get_lines(filename)
+def part1(line):
+    sum = 0
+    for i in range(0, len(line)):
+        prev = i - 1
+        if prev < 0:
+            prev = len(line) - 1
+        if line[prev] == line[i]:
+            sum += int(line[i])
 
-    return 0
+    return sum
 
 
-def part2(filename):
-    lines = io.get_lines(filename)
+def part2(line):
+    sum = 0
+    for i in range(0, len(line)):
+        other = (i + (len(line) // 2)) % len(line)
+        if line[other] == line[i]:
+            sum += int(line[i])
 
-    return 0
+    return sum
 
 
 def main():
-    assert part1("example.txt") == 0
-    print(part1("input.txt"))
+    assert part1("1122") == 3
+    assert part1("1111") == 4
+    assert part1("1234") == 0
+    assert part1("91212129") == 9
 
-    assert part2("example.txt") == 0
-    print(part2("input.txt"))
+    print(part1(io.get_lines("input.txt")[0]))
+
+    assert part2("1212") == 6
+    assert part2("1221") == 0
+    assert part2("123425") == 4
+    assert part2("123123") == 12
+    assert part2("12131415") == 4
+
+    print(part2(io.get_lines("input.txt")[0]))
 
 
 if __name__ == "__main__":
--- template.py
+++ template.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
-from .. import util
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from utils import io
 
 
 def part1(filename):
-    line = util.get_lines(filename)[0]
-
-    print(line)
+    lines = io.get_lines(filename)
 
     return 0
 
 
 def part2(filename):
-    lines = util.get_lines(filename)
+    lines = io.get_lines(filename)
 
     return 0
 
 
 def main():
+    assert part1("example.txt") == 0
     print(part1("input.txt"))
+
+    assert part2("example.txt") == 0
     print(part2("input.txt"))
 
 
