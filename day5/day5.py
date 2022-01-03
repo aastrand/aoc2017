2f7a1cbd6ec971dbf45a26be6edc38fc54178b43
Anders Åstrandd <anders@teknodromen.se>
2022-01-03 17:11:50 +0100

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from utils import io
+
+
+def solve(filename, part2=False):
+    ops = [int(o) for o in io.get_lines(filename)]
+
+    pos = 0
+    steps = 0
+    while pos < len(ops):
+        op = ops[pos]
+
+        if part2 and op >= 3:
+            ops[pos] -= 1
+        else:
+            ops[pos] += 1
+
+        pos += op
+        steps += 1
+
+    return steps
+
+
+def main():
+    assert solve("example.txt") == 5
+    print(solve("input.txt"))
+
+    assert solve("example.txt", True) == 10
+    print(solve("input.txt", True))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
